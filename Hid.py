a45d4de7ffcbc1be134c2e7758468bf173cccd1d
trekimann <trekimann@gmail.com>
2019-06-21 20:56:52 +0100

    fixed hid errors

--- Hid.py
+++ Hid.py
@@ -1,4 +1,4 @@
-from gpiozero import LED as buzz # using LED as for now we just want on and off
+from gpiozero import LED # using LED as for now we just want on and off
 from enum import Enum
 
 class activationPattern(Enum):
@@ -6,12 +6,13 @@ class activationPattern(Enum):
     solid = 2
 
 class hid(object):
-    #def __init__(self):
-    #  self.bearing#the center bearing that triggers this operator
-    #  self.angle#the angle EITHER SIDE of the bearing which also triggers the operator
+    def __init__(self,pin):
+        #self.bearing#the center bearing that triggers this operator
+        #self.angle#the angle EITHER SIDE of the bearing which also triggers the operator
+        self.buzz = LED(pin)
 
-    def activate(pattern):
-        if pattern.name == activationPattern.pulse
-            buzz.blink(background=true, n=5,on_time=0.5,off_time=0.5)
-        else 
-            buzz.blink(background=true,n=1)
+    def activate(self, pattern):
+        if pattern == activationPattern.pulse:
+            self.buzz.blink(background=True, n=5,on_time=0.5,off_time=0.5)
+        else:
+            self.buzz.blink(background=True,n=1)
