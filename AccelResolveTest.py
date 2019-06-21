2caa9e323cbff78c746e1d6786f649a71e091152
trekimann <trekimann@gmail.com>
2019-06-21 18:51:34 +0100

    first math run

--- AccelResolveTest.py
+++ AccelResolveTest.py
@@ -1,8 +1,13 @@
-!#/usr/bin/python3
 import math
 
-AccelX = 0
-AccelY = 0
-AccelZ = 0
+AccelX = 1
+AccelY = 2
+AccelZ = 3
 
 theta = math.atan( AccelX/ (math.sqrt( (AccelY**2)+ (AccelZ**2) )) )
+psi = math.atan( AccelY/ (math.sqrt( (AccelX**2)+ (AccelZ**2) )) )
+omega = math.atan( (math.sqrt( (AccelX**2)+ (AccelY**2) ))/ AccelZ )
+
+print('Theta: '+str(theta))
+print('Psi: '+str(psi))
+print('Omega: '+str(omega))
