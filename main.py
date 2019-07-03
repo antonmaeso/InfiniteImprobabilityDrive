2af39155d880fa2bc1d26c891346a6d53856dbd7
trekimann <trekimann@gmail.com>
2019-07-03 21:41:17 +0100

    added southmotor to main

--- main.py
+++ main.py
@@ -12,13 +12,17 @@ from Hid import activationPattern as ap
 timeToEnd = dt.datetime.now() + dt.timedelta(seconds=60)
 
 currentTime = dt.datetime.now()
-buzzer = motor(18)
+northMotor = motor(18)
+#southMotor = motor()
 while currentTime < timeToEnd:
     if buzzer.isActive() != True:
         heading = Co.bearing([-85.9,47,-97.4,30.3,-200.9,-74.2])
     
         print("Heading: "+"{0:.4f}".format(heading), end = "\r")
         if (heading > 340) or (heading < 20):
-            buzzer.activate(ap.solid)
+            northMotor.activate(ap.solid)
+        # elif (heading > 160) or (heading < 200)
+        #     southMotor.activate(ap.solid)
+
     
         currentTime = dt.datetime.now()
